1fc1212ee2dcc472a91a58810aa2d276a5ed8de9
Korpong Wongpatikaseree <a_m867@hotmail.com>
2011-09-03 12:10:45 +0700

    Add : New Class

---
+++ myOS/NewClass.h
@@ -0,0 +1,13 @@
+//
+//  NewClass.h
+//  myOS
+//
+//  Created by Korpong Wongpatikaseree on 9/3/54 BE.
+//  Copyright 2554 TNS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewClass : NSObject
+
+@end
---
+++ myOS/NewClass.m
@@ -0,0 +1,23 @@
+//
+//  NewClass.m
+//  myOS
+//
+//  Created by Korpong Wongpatikaseree on 9/3/54 BE.
+//  Copyright 2554 TNS. All rights reserved.
+//
+
+#import "NewClass.h"
+
+@implementation NewClass
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end

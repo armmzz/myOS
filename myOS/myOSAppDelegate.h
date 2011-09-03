36d214a4e6509b54681ab153af838025acd7a2da
Korpong Wongpatikaseree <a_m867@hotmail.com>
2011-09-03 11:21:00 +0700

    Initial Commit

---
+++ myOS/myOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  myOSAppDelegate.h
+//  myOS
+//
+//  Created by Korpong Wongpatikaseree on 9/3/54 BE.
+//  Copyright 2554 TNS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myOSAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
